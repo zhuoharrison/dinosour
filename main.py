46bfafe53d6d85c9893875c69e6d03502bad4dce
Harrison <zhuoharrison@gmail.com>
2022-12-16 21:27:33 -0500

    Reduced # of press to 2 for Shift key

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import time
-
 import pyautogui
+
 pyautogui.FAILSAFE = False
 while True:
     time.sleep(15)
-    for i in range(0, 100):
-        pyautogui.moveTo(0, i * 5)
-    for i in range(0, 3):
+    for i in range(0, 50):
+        pyautogui.moveTo(i * 10, i * 10)
+    for i in range(0, 2):
         pyautogui.press('shift')
     for i in range(0, 3):
         pyautogui.press('ctrl')
+
+# print(pyautogui.size())
+# time.sleep(2)
+# pyautogui.scroll(-7000)
+# pyautogui.moveTo(250, 900)
